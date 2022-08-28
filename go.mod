901adf78254dd055c978ae6acc8e0e49b4d1bbd9
firescry <49809580+firescry@users.noreply.github.com>
2022-08-28 14:43:00 +0200

    Implement basic appliance discovery

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/firescry/purematter
 
 go 1.19
+
+require github.com/huin/goupnp v1.0.3
+
+require golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/url"
+
+	"github.com/huin/goupnp"
+)
+
+const (
+	URN_Philips_DiProduct_1 = "urn:philips-com:device:DiProduct:1"
+)
 
 func main() {
-	fmt.Println("Hello, Matter!")
+
+	log.Printf("Looking for known devices...\n")
+	maybeRootDevices, err := goupnp.DiscoverDevices(URN_Philips_DiProduct_1)
+	if err != nil {
+		log.Fatalf("Could not discover %s devices: %v\n", URN_Philips_DiProduct_1, err)
+	}
+
+	locations := make([]*url.URL, 0, len(maybeRootDevices))
+	log.Printf("Found %d device(s):\n", len(maybeRootDevices))
+	for _, maybeRootDevice := range maybeRootDevices {
+		if maybeRootDevice.Err != nil {
+			log.Fatalf("  Failed to probe device at %s\n", maybeRootDevice.Location.String())
+		} else {
+			locations = append(locations, maybeRootDevice.Location)
+			log.Printf("  Successfully probed device at %s\n", maybeRootDevice.Location.String())
+		}
+	}
+
+	log.Printf("Attempt to re-acquire %d device(s):\n", len(locations))
+	for _, location := range locations {
+		if _, err := goupnp.DeviceByURL(location); err != nil {
+			log.Fatalf("  Failed to reacquire device at %s: %v\n", location.String(), err)
+		} else {
+			log.Printf("  Successfully reacquired device at %s\n", location.String())
+		}
+	}
+
 }
