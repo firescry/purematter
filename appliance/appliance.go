002fd8c525691962e3bdbd7d16667dec01e695ea
firescry <49809580+firescry@users.noreply.github.com>
2022-09-04 15:10:35 +0200

    Set endpoints for appliance

--- appliance/appliance.go
+++ appliance/appliance.go
@@ -1,23 +1,35 @@
 package appliance
 
 import (
+	"log"
+
+	"github.com/firescry/purematter/client"
+
 	"github.com/huin/goupnp"
 )
 
 type Appliance struct {
-	host         string
 	Manufacturer string
 	ModelName    string
 	ModelNumber  string
 	UUID         string
+
+	endpoints *map[string]string
 }
 
 func NewAppliance(dev goupnp.RootDevice) *Appliance {
+	host := dev.URLBase.Hostname()
+	endpoints, err := client.GenerateEndpoints(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return &Appliance{
-		host:         dev.URLBase.Hostname(),
 		Manufacturer: dev.Device.Manufacturer,
 		ModelName:    dev.Device.ModelName,
 		ModelNumber:  dev.Device.ModelNumber,
 		UUID:         dev.Device.UDN,
+
+		endpoints: endpoints,
 	}
 }
---
+++ client/endpoints.go
@@ -0,0 +1,30 @@
+package client
+
+import "net/url"
+
+var (
+	DefaultEndpoints = map[string]string{
+		"security": "http://0.0.0.0/di/v1/products/0/security",
+	}
+)
+
+func GenerateEndpoints(host string) (*map[string]string, error) {
+	endpoints := map[string]string{}
+	for endpoint, url := range DefaultEndpoints {
+		newUrl, err := UpdateEndpointHost(host, url)
+		if err != nil {
+			return nil, err
+		}
+		endpoints[endpoint] = newUrl
+	}
+	return &endpoints, nil
+}
+
+func UpdateEndpointHost(host, endpoint string) (string, error) {
+	e, err := url.Parse(endpoint)
+	if err != nil {
+		return "", err
+	}
+	e.Host = host
+	return e.String(), nil
+}
